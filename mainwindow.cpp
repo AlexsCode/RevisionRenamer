f8caf91969a82c0ca5e7e9690f0b9a07879a4d9a
AlexsCode <61622354+AlexsCode@users.noreply.github.com>
2020-05-11 23:02:52 +0100

    Update mainwindow.cpp
    V4 Code Working with comments added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -55,32 +55,28 @@ void MainWindow::on_pushButton_Rename_clicked()
     //stepper to compare values
     for (int j=0;j<file_names.size();j++)
          for (int i=0;i<splitbyline.size();i++)
-             {  QFile file(file_names[j]); //THIS IS IT
-                QFileInfo targetfileinfo(file.fileName());
-                QFileInfo sourcepath(targetfileinfo.path());
-                QString singlefilename(targetfileinfo.fileName());
-                QString FileType(targetfileinfo.completeSuffix());//Allows for many filetypes.
-                QString Filename_notype (targetfileinfo.baseName()) ;
-                //QString Filename_notype = singlefilename.remove(FileType);
+             {  QFile file(file_names[j]); //Creates and assigns file from filepath
+                QFileInfo targetfileinfo(file.fileName()); //finds the file name
+                QFileInfo sourcepath(targetfileinfo.path()); //finds the file path
+                QString singlefilename(targetfileinfo.fileName()); // unsure if still required|
+                QString FileType(targetfileinfo.completeSuffix());//Saves filetype |Allows for many filetypes.
+                QString Filename_notype (targetfileinfo.baseName()); //Saves filename without the filetype
 
 
-                if(splitbyline[i].contains(Filename_notype))
-                 {   QString Revision = (splitbyline[i].right(3));
-                     //QString FileType(targetfileinfo.completeSuffix());
-                     QString Copyvalue= (Filename_notype+"_"+Revision+"."+FileType);//name of the copy
+                if(splitbyline[i].contains(Filename_notype)) // searches if finalname is in the text input
+                 {   QString Revision = (splitbyline[i].right(3)); //splits the revision from the end of the substring splitbyline
+                     QString Copyvalue= (Filename_notype+"_"+Revision+"."+FileType);//sets what the copy should be named
 
                      //test outputs
 
-                     ui->comboBox_output->addItem(Revision);
-                     ui->comboBox_output->addItem(file_names[j]);
+                     ui->comboBox_output->addItem(Revision); //Allows for correctly displaying Revision|can be removed
+                     ui->comboBox_output->addItem(file_names[j]);//Allows for correctly displaying file paths |can be removed
 
-                     QString Path (sourcepath.filePath());
-                     QString targetPath = (Path+"/"+Copyvalue);
-                     ui->textEdit_output->setText(targetPath);
+                     QString Path (sourcepath.filePath()); //finds the full path
+                     QString targetPath = (Path+"/"+Copyvalue); //adds the path to the prefered name
+                     ui->textEdit_output->setText(targetPath); // Show the exact new path+name.|can be removed
 
-                     file.copy(targetPath);
-
-                     //QFile copy is broken
+                     file.copy(targetPath); //Actual copy Operation assuming if is true.
 
                    }
               }
